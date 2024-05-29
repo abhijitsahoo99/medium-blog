3073ebe679271bbe8d21491f846d9b189fa08a0d
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2024-05-29 14:05:33 +0530

    update: appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,6 +1,8 @@
 import Logo from '../assets/logo.svg'
+import { useNavigate } from 'react-router-dom';
 
 function Appbar() {
+    const navigate = useNavigate();
     return (
       <>
         <nav className="py-3 px-28 border-b-[2px] bg-white">
@@ -10,11 +12,11 @@ function Appbar() {
                     Medium
                 </div>
                 <div className="font-normal text-sm space-x-6">
-                    <span className="text-black cursor-pointer">Our Story</span>
-                    <span className="text-black cursor-pointer">Membership</span>
-                    <span className="text-black cursor-pointer">Write</span>
-                    <span className="text-black cursor-pointer">Sign in</span>
-                    <button className="border-solid border-1 rounded-3xl bg-black text-white font-normal px-4 py-2">
+                    <span className="text-black cursor-pointer hover:border-b-2">Our Story</span>
+                    <span className="text-black cursor-pointer hover:border-b-2">Membership</span>
+                    <span className="text-black cursor-pointer hover:border-b-2">Write</span>
+                    <span className="text-black cursor-pointer hover:border-b-2" onClick={() => navigate('/signin')}>Sign in</span>
+                    <button className="border-solid border-1 rounded-3xl bg-black text-white font-normal px-4 py-2" onClick={() => navigate('/signup')}>
                         Get Started
                     </button>
                 </div>
